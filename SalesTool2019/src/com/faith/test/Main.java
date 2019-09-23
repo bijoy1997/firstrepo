691205de1eb0b26a5ed50d5ce814b96827cb20dd
Bijoy Jose <Bijoy Jose@Bijoy>
2019-09-23 15:14:16 +0530

    Initial commit

---
+++ SalesTool2019/src/com/faith/test/Main.java
@@ -0,0 +1,17 @@
+package com.faith.test;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SalesData obj=new SalesData();
+		System.out.println("Hello Ust");
+		displayGreetings();
+		obj.dispaly();
+
+	}
+	public static void displayGreetings(){
+		System.out.println("Welcome to sales app");
+	}
+
+}
---
+++ SalesTool2019/src/com/faith/test/SalesData.java
@@ -0,0 +1,8 @@
+package com.faith.test;
+
+public class SalesData {
+	void dispaly(){
+		System.out.println("sales report..");
+	}
+
+}
